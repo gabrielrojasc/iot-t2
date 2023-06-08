199f9e4920407d413efd0a00e8364567a5f9e889
Gabriel Rojas <gabo06123@gmail.com>
2023-06-07 22:14:52 -0400

    --wip-- [skip ci]

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -21,6 +21,12 @@
 
 #define GATTS_TAG "GATTS_DEMO"
 
+// 10 == configuracion
+// 30 == continuo
+// 31 == discontinuo
+int status = 10;
+char protocol = '0';
+
 /// Declare the static function
 static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
 
@@ -377,11 +383,9 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
     esp_gatt_rsp_t rsp;
     memset(&rsp, 0, sizeof(esp_gatt_rsp_t));
     rsp.attr_value.handle = param->read.handle;
-    rsp.attr_value.len = 4;
-    rsp.attr_value.value[0] = 0xde;
-    rsp.attr_value.value[1] = 0xed;
-    rsp.attr_value.value[2] = 0xbe;
-    rsp.attr_value.value[3] = 0xef;
+    rsp.attr_value.len = 2;
+    rsp.attr_value.value[0] = protocol;
+    rsp.attr_value.value[1] = status;
     esp_ble_gatts_send_response(gatts_if, param->read.conn_id, param->read.trans_id,
                                 ESP_GATT_OK, &rsp);
     break;
@@ -393,6 +397,8 @@ static void gatts_profile_a_event_handler(esp_gatts_cb_event_t event, esp_gatt_i
     {
       ESP_LOGI(GATTS_TAG, "GATT_WRITE_EVT, value len %d, value :", param->write.len);
       esp_log_buffer_hex(GATTS_TAG, param->write.value, param->write.len);
+      protocol = param->write.value[0];
+      status = param->write.value[1];
       if (gl_profile_tab[PROFILE_A_APP_ID].descr_handle == param->write.handle && param->write.len == 2)
       {
         uint16_t descr_value = param->write.value[1] << 8 | param->write.value[0];
--- raspberry/main.py
+++ raspberry/main.py
@@ -1,95 +1,40 @@
-import pygatt
 from time import sleep
 from struct import pack
+from gattlib import GATTRequester
 
-from unpacking import parse_data
-from db import get_configs
-
-# Definir los UUIDs de los servicios y características del dispositivo BLE
 DEVICE_ADDRESS = "4C:EB:D6:62:18:3A"
 SERVICE_UUID = "000000FF-0000-1000-8000-00805f9b34fb"
 CHARACTERISTIC_UUID = "0000FF01-0000-1000-8000-00805f9b34fb"
+READ_HANDLE = 42
+WRITE_HANDLE = 43
 
-# Definir los estados de la máquina de estado
-STATE_DISCONNECTED = 0
-STATE_CONNECTING = 1
-STATE_CONNECTED = 2
 
 # Inicializar la conexión PyGatt y el estado inicial de la máquina de estado
-adapter = pygatt.GATTToolBackend()
-state = STATE_DISCONNECTED
 
 
-# Función para manejar eventos de conexión
-def handle_connection_event(event):
-    global state
-    if event == "disconnected":
-        print("El dispositivo se desconectó")
-        state = STATE_DISCONNECTED
-    elif event == "connected":
-        print("El dispositivo se conectó")
-        state = STATE_CONNECTED
+class MyRequester(GATTRequester):
+    ...
 
 
-# Función para conectar al dispositivo BLE
-def connect():
-    global state
-    state = STATE_CONNECTING
-    device = adapter.connect(
-        DEVICE_ADDRESS,
-        address_type=pygatt.BLEAddressType.random,
-        timeout=10,
-        auto_reconnect=True,
-    )
-    device.subscribe(CHARACTERISTIC_UUID, callback=handle_notification)
-
-    return device
+req = MyRequester(DEVICE_ADDRESS, False)
 
 
-# Función para desconectar del dispositivo BLE
-def disconnect():
-    global state
-    state = STATE_DISCONNECTED
-    adapter.stop()
-
-
-# Función para manejar notificaciones de características
-def handle_notification(handle, value):
-    print("Notificación recibida:", value.hex())
+def get_config_packet(protocol, status):
+    return pack("<2c", protocol.encode(), status.encode())
 
 
 # Bucle principal de la máquina de estado
 while True:
-    device = None
-    if state == STATE_DISCONNECTED:
-        # Si el estado actual es desconectado, intentar conectar
-        try:
-            device = connect()
-        except:
-            continue
-    elif state == STATE_CONNECTING:
-        # Si el estado actual es conectando, esperar hasta que se conecte o se agote el tiempo de espera
-        try:
-            adapter.start()
-            sleep(1)
-        except pygatt.exceptions.BLEError:
-            pass
-    elif state == STATE_CONNECTED and device is not None:
-        # Si el estado actual es conectado, hacer cualquier operación necesaria en el dispositivo
-        # transport_layer=10 -> Config
-        # transport_layer=30 -> Continua
-        # transport_layer=31 -> Discontinua
-        for protocol, transport_layer in get_configs():
-            print(f"Sending config: {protocol=}, {transport_layer}")
-            packet = pack("<2c", str(protocol).encode(), str(transport_layer).encode())
-            device.char_write(CHARACTERISTIC_UUID, packet, wait_for_response=True)
-
-            print(f"Reading data...")
-            raw_data = device.char_read(CHARACTERISTIC_UUID)
-            print(f"Raw data received: {raw_data}")
-
-            data = parse_data(raw_data, protocol)
-            print(f"Data parse: {data}")
-
-
+    try:
+        if not req.is_connected():
+            req.connect(True)
+        else:
+            print("connected")
+    except Exception:
+        print("Error de conexión")
         sleep(1)
+        continue
+
+    req.write_by_handle(WRITE_HANDLE, get_config_packet("1", chr(30)))
+    print(req.read_by_handle(READ_HANDLE))
+    sleep(1)
