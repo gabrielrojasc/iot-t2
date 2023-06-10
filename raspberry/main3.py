747a73738e31d03214917fea754900e18bfa4862
Gabriel Rojas <gabo06123@gmail.com>
2023-06-10 00:43:12 -0400

    --wip-- [skip ci]

--- raspberry/main3.py
+++ raspberry/main3.py
@@ -56,7 +56,7 @@ class StateMachine(GATTHelper):
     def __init__(self, status, protocol):
         self.status = status
         self.protocol = protocol
-        self.state = State.DISCONNECTED
+        self.set_state(State.DISCONNECTED)
         self.loop = asyncio.get_event_loop()
         self.device_address = "4C:EB:D6:62:18:3A"
         self.characteristic_uuid = "0000FF01-0000-1000-8000-00805f9b34fb"
@@ -66,7 +66,6 @@ class StateMachine(GATTHelper):
 
     def start(self):
         while True:
-            logger.info(f"Current state: {self.state}")
             if self.state == State.FINISHED:
                 break
             method = getattr(self, f"{self.state.value.lower()}_state")
@@ -78,7 +77,7 @@ class StateMachine(GATTHelper):
     async def disconnected_state_async(self):
         logger.info(f"Disconnected. Connecting to device: {self.device_address}")
         self.client = BleakClient(self.device_address)
-        self.state = State.CONNECTING
+        self.set_state(State.CONNECTING)
 
     def connecting_state(self):
         self.loop.run_until_complete(self.connecting_state_async())
@@ -100,12 +99,11 @@ class StateMachine(GATTHelper):
         self.connection_attempts += 1
         try:
             await self.client.connect()
-            self.state = successful_state
+            self.set_state(successful_state)
             self.time_to_connect += time.time() - t1
             self.save_loss()
         except Exception as e:
-            logger.warning("Error connecting to device: {}".format(e))
-            self.state = error_state
+            self.set_state(error_state)
             self.time_to_connect += time.time() - t1
 
     def save_loss(self):
@@ -115,27 +113,27 @@ class StateMachine(GATTHelper):
 
     def configuration_state(self):
         self.write_gatt_char(get_config_packet(self.status, self.protocol))
-        self.state = State.SUBSCRIBING
+        self.set_state(State.SUBSCRIBING)
 
     def subscribing_state(self):
         try:
             self.susbscribe_gatt_char(self.notify_callback)
-            self.state = State.CONNECTED
             logger.info("Subscribed to device")
+            self.set_state(State.CONNECTED)
         except Exception as e:
-            logger.info(f"Error subscribing to device: {e}")
-            self.state = State.RECONNECTING
+            self.set_state(State.RECONNECTING)
 
     def connected_state(self):
         if not self.client.is_connected:
-            self.state = State.RECONNECTING
+            self.set_state(State.RECONNECTING)
+
         self.go_to_sleep(1)
         if self.data_ready:
             data = self.read_gatt_char()
             logger.info(f"Received data: {data}")
             self.packets_received += 1
             if self.packets_received >= 3:
-                self.state = State.DISCONNECTING
+                self.set_state(State.DISCONNECTING)
             try:
                 parsed_data = parse_data(data, int(self.protocol))
                 logger.info(f"Parsed data: {parsed_data}")
@@ -151,7 +149,7 @@ class StateMachine(GATTHelper):
         await asyncio.sleep(seconds)
 
     def notify_callback(self, sender, data):
-        logger.info(f"{sender=}, {data=}")
+        logger.info("Received notification")
         self.data_ready = True
 
     def disconnecting_state(self):
@@ -160,14 +158,20 @@ class StateMachine(GATTHelper):
     async def disconnecting_state_async(self):
         await self.write_gatt_char_wait_for_config()
         self.packets_received = 0
-        self.state = State.FINISHED
+        self.set_state(State.FINISHED)
         await self.client.disconnect()
 
     async def write_gatt_char_wait_for_config(self):
         await self.write_gatt_char_async(get_config_packet(10, "0"))
 
+    def set_state(self, state):
+        self.set_state(state)
+        logger.info("Current state: {}".format(self.state))
+
 
 if __name__ == "__main__":
-    for protocol, status in db.get_configs():
+    for protocol, status in (3, 31):  # db.get_configs():
+        logger.info(f"Starting with {protocol=}, {status=}")
+        print()
         sm = StateMachine(status, str(protocol))
         sm.start()
